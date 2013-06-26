bc6cb9e97ae676ce73600abb9445f66f2f54a769
albyvezzani@gmail.com <albyvezzani@gmail.com@a1debe9a-6b89-db7c-54c9-71a688ab7d58>
2013-06-26 23:08:56 +0000

    ho inserito l'heapsort

--- trunk/comprimi.cc
+++ trunk/comprimi.cc
@@ -29,22 +29,67 @@ bool conta_occorrenze(char sorgente[], queue &coda){
 		return false;
 	while(f.read(reinterpret_cast<char *>(&car), sizeof (car)))
 		coda.elemento[static_cast<int>(car)+1]->occorrenze++;
-	coda.n_elementi=CARATTERI_ASCII-1;
+	coda.n_elementi=CARATTERI_ASCII-1	;
 	return true;
 }
 
 
+void scambio(pnode &a,pnode &b)
+{
+pnode p=a;
+a=b;
+b=p;
+}
+
+
+void MinHeapify(queue &coda, int j, int n)
+{
+int k=j;
+if ((2*j+1<=n) && (coda.elemento[2*j+1]->occorrenze<coda.elemento[k]->occorrenze))
+	k=2*j+1;
+if (2*j<=n && coda.elemento[2*j]->occorrenze<coda.elemento[k]->occorrenze)
+	k=2*j;
+if (k!=j)
+	{
+	scambio (coda.elemento[j],coda.elemento[k]);
+	MinHeapify(coda, k, n);
+	}
+}
+
+
+void BuildMinHeap(queue &coda)
+{
+for (int i=coda.n_elementi/2;i>=1;i--)
+	MinHeapify(coda,i,coda.n_elementi);
+}
+
+
+void HeapSort(queue &coda)
+{
+for (int i=coda.n_elementi;i>1;i--)
+	{
+	scambio(coda.elemento[1],coda.elemento[i]);
+	MinHeapify(coda,1,i-1);
+	}
+}
+
+void pulisci_coda(queue &coda)
+{
+BuildMinHeap(coda);
+HeapSort(coda);
+while(coda.elemento[coda.n_elementi]->occorrenze==0)
+	coda.n_elementi--;
+}
+
 bool comprimi (char sorgente[], char destinazione[]){
 	queue coda;
-	cout<<"provo a inizializzare la coda"<<endl;
 	inizializza_coda(coda);
-	cout<<"provo a contare"<<endl;
 	conta_occorrenze(sorgente, coda);
-	cout<<"provo a stampare"<<endl;
-	for(int i=1;i<coda.n_elementi;i++){
+	pulisci_coda(coda);
+	for(int i=1;i<=coda.n_elementi;i++){
 		cout<<coda.elemento[i]->carattere<<"\t"<<coda.elemento[i]->occorrenze<<endl;
 	}
-/*pulisci_coda(coda);
+/*pulisci_coda(coda); FATTO
 int num_caratteri=coda.n_elementi;
 crea_albero(coda);
 scrivi_file_compresso(destinazione, coda, num_caratteri); //contiene esplorazione dell'albero, scrittura del file compresso, etc..*/
