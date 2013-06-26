b8a9ee0d40420d9750986071b769311163257ca3
marco.torre1992@gmail.com <marco.torre1992@gmail.com@a1debe9a-6b89-db7c-54c9-71a688ab7d58>
2013-06-26 07:11:25 +0000

    modificate fun in comprimi

--- trunk/comprimi.cc
+++ trunk/comprimi.cc
@@ -2,11 +2,30 @@
 #include <fstream>
 #include "struttura_dati.h"
 
-bool comprimi (char sorgente[], char destinazione[])
-{
-queue coda;
-coda.n_elementi=0;
-conta_occorrenze(sorgente, coda);
+
+/** Inizializzazione della coda
+ *
+ * La funzione prende in ingresso una coda e la inizializza a 0 elementi.
+ */
+void inizializza_coda(queue coda){
+	coda.n_elementi=0;
+}
+
+/** Conta le occorrenze di ogni lettera.
+ *
+ * Prende in ingresso il file sorgente e conta le occorrenze di tutte le
+ * lettere.
+ */
+void conta_occorrenze(char sorgente[], queue coda){
+
+}
+
+
+bool comprimi (char sorgente[], char destinazione[]){
+	queue coda;
+	inizializza_coda(coda);
+
+	conta_occorrenze(sorgente, coda);
 pulisci_coda(coda);
 int num_caratteri=coda.n_elementi;
 crea_albero(coda);
