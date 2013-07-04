2a5f4550cc8682bd018d6dcb916ebfe99f9f06fa
marco.torre1992@gmail.com <marco.torre1992@gmail.com@a1debe9a-6b89-db7c-54c9-71a688ab7d58>
2013-07-04 14:57:01 +0000

    Corretta la documentazione, aggiornato il makefile nascondendo il debug

--- src/Huffman.cc
+++ src/Huffman.cc
@@ -1,4 +1,4 @@
-/** Programma principale.
+/**
  * @mainpage Programma di compressione e decompressione file.
  *
  * Programma per la compressione e la decompressione dei File mediante
@@ -43,86 +43,87 @@ extern "C" void hide_main_window(GtkButton *button, gpointer data){
  *
  * Questa funzione visualizza una finestra dialogo con l'utente che
  * comunica la riuscita o meno di una certa operazione.
+ * Il parametro c1 indica la riuscita o no dell'operazione mentre
+ * il flag_cmp indica se l'operazione è una compressione o una
+ * decompressione.
  */
 static void mostra_messaggi(bool c1,int flag_cmp){
 	GtkWidget *ins_dialog=GTK_WIDGET(gtk_builder_get_object(builder,"messaggi_utente"));
 	GtkLabel *label=GTK_LABEL(gtk_builder_get_object(builder,"messaggio"));
 	GtkImage *image=GTK_IMAGE(gtk_builder_get_object(builder,"img_result"));
-	if(flag_cmp)
-	{
-		if(c1)
-		{
+	if(flag_cmp){
+		if(c1){
 			gtk_label_set_text(label,"Compressione del file riuscita");
 			gtk_image_set_from_stock(image,"gtk-yes",  GTK_ICON_SIZE_BUTTON);
-		}
-		else
-		{
+			}
+		else{
 			gtk_label_set_text(label,"Compressione del file non riuscita");
 			gtk_image_set_from_stock(image,"gtk-dialog-warning",  GTK_ICON_SIZE_BUTTON);
+			}
 		}
-	}
-	else
-	{
-		if(c1)
-		{
+	else{
+		if(c1){
 			gtk_label_set_text(label,"Decompressione del file riuscita");
 			gtk_image_set_from_stock(image,"gtk-yes",  GTK_ICON_SIZE_BUTTON);
-		}
-		else
-		{
+			}
+		else{
 			gtk_label_set_text(label,"Decompressione del file non riuscita");
 			gtk_image_set_from_stock(image,"gtk-dialog-warning",  GTK_ICON_SIZE_BUTTON);
+			}
 		}
-	}
-	
-	gtk_widget_show_all(ins_dialog);
-	
+	gtk_widget_show_all(ins_dialog);	
 }
-/** segnale che gestisce l'evento click del pulsante comprimi 
-* apre la finestra di dialogo adibita al salvataggio
-*/
-extern "C" void handler_comprimi(GtkButton *button,gpointer data)
-{	
+
+
+/** Segnale del bottone compressore.
+ *
+ * Segnale che gestisce l'evento click del pulsante compressore: 
+ * apre la finestra di dialogo adibita al salvataggio del file compresso,
+ * ricava il percorso di salvataggio e comunica lo stato dell'operazione.
+ */
+extern "C" void handler_comprimi(GtkButton *button,gpointer data){	
 	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(builder,"mex_esegui")),"Comprimi");
 	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(builder,"msg")),"scegli dove salvare e il nome del file compresso");
-	
 	gchar *sorg=gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(data));
-	if(sorg==NULL)
-	{		
+	if(sorg==NULL){		
 		mostra_messaggi(false,1);
 		return;
-	}
+		}
 	GtkWidget *salvataggio=GTK_WIDGET(gtk_builder_get_object(builder,"salva"));
 	gtk_widget_show_all(salvataggio);
 }
-/** chiude la finestra adibita al salvataggio */
-static void chiudi_salvataggio()
-{
+
+
+/** Chiude la finestra adibita al salvataggio.
+ *
+ * Nasconde la finestra di salvataggio.
+ */
+static void chiudi_salvataggio(){
 	GtkWidget *salvataggio=GTK_WIDGET(gtk_builder_get_object(builder,"salva"));
 	gtk_widget_hide(salvataggio);
 }
-/** crea il percorso assouluto al file creato agganciando a una stessa stringa
-* il percorso del file, il suo nome, e l'estensione adeguata all'operazione in 
-* corso
-*/
-static char * crea_destinazione(gpointer data,bool flag)
-{
+
+
+/** Ottiene il percorso completo di salvataggio.
+ *
+ * Ricava per prima cosa il percorso selezionato dall'utente nel file_chooser,
+ * in seguito ricava il nome che l'utente vuole assegnare al file elaborato,
+ * infine crea una stringa e vi inserisce il percorso, aggiunge uno /,
+ * aggiunge il nome file poi l'estensione in base al flag che segnala se è
+ * in fase di compressione o di decompressione.
+ * Es: PERCORSO/NOMEFILE.ESTENSIONE
+ */
+static char * crea_destinazione(gpointer data,bool flag){
 	char *percorso=gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(data));
 	if(percorso==NULL)
 		return NULL;
-	//cout<<"percorso: "<<percorso<<endl;
-	
 	GtkEntry *entry=GTK_ENTRY(gtk_builder_get_object(builder,"nome_file"));
 	const char *testo_file=gtk_entry_get_text(entry);
-	//cout<<"nome file: "<<nome_file<<endl;
 	istringstream s(testo_file);
 	char *nome_file=new char [strlen(testo_file)];
 	s>>nome_file;
-	//cout<<nome_file<<endl;
-	if((nome_file[0]<'A' || nome_file[0]>'z'))
-	{		
+	if(nome_file[0]<'A' || nome_file[0]>'z')		
 		return NULL;
-	}
 	char *dest=new char[strlen(percorso)+strlen(nome_file)+4];
 	strcpy(dest,percorso);
 	strcat(dest,"/");
@@ -134,89 +135,79 @@ static char * crea_destinazione(gpointer data,bool flag)
 		
 	return dest;
 }
-/** funzione che effettua la compressione del .txt selezionato 
-* sfrutta la funzione ::crea_destinazione che crea il percorso assoluto
-* per trovare dove salvare il file compresso con nome scelto dall'utente.
-* Utilizza la funzione ::comprimi per effettuare tutte le operazioni
-* relative alla compressione del file .txt
-*/
-static void effettua_compressione(gpointer data)
-{
-	gchar *sorg=gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(gtk_builder_get_object(builder,"chooser_compressore")));
-	cout<<"sorgente"<<sorg<<endl;
-	
+
+
+/** Effettua la compressione del file.
+ * 
+ * La funzione effettua la compressione del .txt selezionato 
+ * sfrutta la funzione ::crea_destinazione che crea il percorso assoluto
+ * in cui salvare il file compresso con il nome scelto dall'utente.
+ * Utilizza la funzione ::comprimi per effettuare tutte le operazioni
+ * relative alla compressione del file.
+ */
+static void effettua_compressione(gpointer data){
+	gchar *sorg=gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(gtk_builder_get_object(builder,"chooser_compressore")));	
 	char *dest=crea_destinazione(data,1);
-	
-	if(dest==NULL)
-	{
+	if(dest==NULL){
 		chiudi_salvataggio();
 		mostra_messaggi(false,1);
 		return;
-	}
-	//cout<<"dest: "<<dest<<endl;
-	
-	D1(cout<<"Chiamo comprimi, gli passo sorgente = "<<sorg<<" e destinazione = "<<dest<<endl);
-	
+		}	
 	bool c1=comprimi(sorg,dest);
-	
 	chiudi_salvataggio();
-	
 	mostra_messaggi(c1,1);
 }
-/** segnale che gestisce l'evento click del pulsante decomprimi
-* questo segnale apre un finestra di dialogo dove poter salvare 
-* e dare un nome al file che si crea con le decompressione
-* controlla inoltre che l'utente abbia selezionato un file .huf 
-* per da decomprimere
-*/
-extern "C" void handler_decomprimi(GtkButton *button,gpointer data)
-{
+
+
+/** Segnale del bottone decompressore.
+ *
+ * Segnale che gestisce l'evento click del pulsante decompressore: 
+ * apre la finestra di dialogo adibita al salvataggio del file decompresso,
+ * ricava il percorso di salvataggio e comunica lo stato dell'operazione.
+ */
+extern "C" void handler_decomprimi(GtkButton *button,gpointer data){
 	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(builder,"mex_esegui")),"Decomprimi");
 	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(builder,"msg")),"scegli dove salvare e il nome del file decompresso");
-	
 	gchar *sorg=gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(data));
-	if(sorg==NULL)
-	{		
+	if(sorg==NULL){		
 		mostra_messaggi(false,0);
 		return;
-	}
+		}
 	GtkWidget *salvataggio=GTK_WIDGET(gtk_builder_get_object(builder,"salva"));
 	gtk_widget_show_all(salvataggio);
 }
-/** funzione che effettua la decompressione del .huf selezionato 
-* sfrutta la funzione ::crea_destinazione che crea il percorso assoluto
-* per trovare dove salvare il file decompresso con nome scelto dall'utente.
-* Utilizza la funzione ::decomprimi per effettuare tutte le operazioni
-* relative alla decompressione del file .huf
-*/
-static void effettua_decompressione(gpointer data)
-{
+
+
+/** Effettua la decompressione del file.
+ * 
+ * La funzione effettua la decompressione del .huf selezionato 
+ * sfrutta la funzione ::crea_destinazione che crea il percorso assoluto
+ * in cui salvare il file compresso con il nome scelto dall'utente.
+ * Utilizza la funzione ::comprimi per effettuare tutte le operazioni
+ * relative alla decompressione del file.
+ */
+static void effettua_decompressione(gpointer data){
 	gchar *sorg=gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(gtk_builder_get_object(builder,"chooser_decompressore")));
-	cout<<"sorgente"<<sorg<<endl;
-	
 	char *dest=crea_destinazione(data,0);
-	
-	if(dest==NULL)
-	{
+	if(dest==NULL){
 		chiudi_salvataggio();
 		mostra_messaggi(false,0);
 		return;
-	}
-	//cout<<"dest: "<<dest<<endl;
-	
-	D1(cout<<"Chiamo comprimi, gli passo sorgente = "<<sorg<<" e destinazione = "<<dest<<endl);
-		
+		}	
 	bool c2=decomprimi(sorg,dest);
-	
 	chiudi_salvataggio();
-	
 	mostra_messaggi(c2,0);
 }
-/** segnale paragonbile a un front-end permette di decidere quale operazione eseguire 
-* se la compressione o la decompressione del file
-*/
-extern "C" void handler_scegli_op(GtkButton *button,gpointer data)
-{
+
+
+/** Sceglie l'operazione da eseguire.
+ *
+ * In base all'operazione scelta richiama la funzione corretta
+ * (::effettua_compressione o ::effettua_decompressione). Per
+ * sapere quale eseguire controlla lo stato della label del
+ * bottone nella finestra di salvataggio.
+ */
+extern "C" void handler_scegli_op(GtkButton *button,gpointer data){
 	GtkLabel *label=GTK_LABEL(gtk_builder_get_object(builder,"mex_esegui"));
 	const char *testo=gtk_label_get_text(label);
 	if(strcmp(testo,"Comprimi")==0)
@@ -224,52 +215,58 @@ extern "C" void handler_scegli_op(GtkButton *button,gpointer data)
 	else
 		effettua_decompressione(data);
 }
-/** link alla documentazione */
-extern "C" void handler_carica_documentazione(GtkMenuItem *item,gpointer data)
-{
+
+
+/** Link alla documentazione
+ *
+ * Permette di aprire una pagina di Firefox con la relativa documentazione
+ * in formato HTML.
+ */
+extern "C" void handler_carica_documentazione(GtkMenuItem *item,gpointer data){
 	system("firefox ../doc/html/index.html");
 }
+
+
 int main(int argc,char *argv[]){
 
 
-	/**inizializzazione gtk */
+	/* Inizializzazione gtk. */
 	gtk_init(&argc,&argv);
-	
 	builder=gtk_builder_new();
 	
-	/** aggiunga interfaccia grafica grafica.glade */
+	
+	/* Aggiunge l'interfaccia grafica creata con Glade */
 	gtk_builder_add_from_file(builder,"grafica.glade",NULL);
 	
-	/** aggiunta dei filtri per la selzione dei file ai filechooser, i filtri sono stati creati tramite glade
-	* qui avviene solo il settaggio del relativo nome e l'aggangio dei filtri ai filechooser
-	*/
+	
+	/* Creazione primo filtro.
+	 * 
+	 * Ricava un puntatore al filtro creato con Glade e lo imposta per accettare i .txt .
+	 * Ricava un puntatore al file_chooser e in seguito aggiunge il filtro al file_chooser.
+	 */
 	GtkFileFilter *filtro=GTK_FILE_FILTER(gtk_builder_get_object(builder,"filtro_file"));
-	/** primo filtri riferito al chooser del compressore rende selezionabili solo i file 
-	* con estensione .txt 
-	*/
 	gtk_file_filter_set_name(filtro,".txt");
-	
-	GtkFileFilter *filtro_dec=GTK_FILE_FILTER(gtk_builder_get_object(builder,"filtro_file2"));
-	/** secondo filtro riferito al chooser del decompressore rende selezionabili solo i file 
-	* con estensione .huf
-	*/
-	gtk_file_filter_set_name(filtro_dec,"*.huf");
-	
-	/** aggancio primo filtro */
 	GtkFileChooser *chooser=GTK_FILE_CHOOSER(gtk_builder_get_object(builder,"chooser_compressore"));
 	gtk_file_chooser_add_filter(chooser,filtro);
 	
-	/** agggancio secondo filtro */
+	
+	/* Creazione secondo filtro.
+	 * 
+	 * Ricava un puntatore al filtro creato con Glade e lo imposta per accettare i .huf .
+	 * Ricava un puntatore al file_chooser e in seguito aggiunge il filtro al file_chooser.
+	 */
+	GtkFileFilter *filtro_dec=GTK_FILE_FILTER(gtk_builder_get_object(builder,"filtro_file2"));
+	gtk_file_filter_set_name(filtro_dec,"*.huf");
 	GtkFileChooser *chooser_dec=GTK_FILE_CHOOSER(gtk_builder_get_object(builder,"chooser_decompressore"));
 	gtk_file_chooser_add_filter(chooser_dec,filtro_dec);
 	
 	
-	/** conessione dei segnali con l'interfaccia */
+	/* Connessione dei segnali con l'interfaccia */
 	gtk_builder_connect_signals(builder,NULL);
 	
-	/** loop principale */
+	
+	/* Loop principale */
 	gtk_main();
 
 	return 0;
-	//return (static_cast<int>(c1 && c2));
 }
--- src/decomprimi.cc
+++ src/decomprimi.cc
@@ -10,6 +10,7 @@
  * al numero di caratteri diversi presenti nel file originale.
  */
 int numero_caratteri(istream &f4){
+	D1(cout<<"sono entrato in numero_caratteri"<<endl);
 	unsigned char car;
 	f4.read(reinterpret_cast<char *>(&car), sizeof (car));
 	return (static_cast<int>(car));
@@ -21,6 +22,7 @@ int numero_caratteri(istream &f4){
  * La funzione legge dal file sorgente i caratteri presenti nel file originale.
  */
 unsigned char *ricava_caratteri (istream &f4, int n_caratteri){
+	D1(cout<<"sono entrato in ricava_caratteri, n_caratteri = "<<n_caratteri<<endl);
 	unsigned char *car = new unsigned char [n_caratteri];
 	for (int i=0;i<n_caratteri;i++)
 		f4.read(reinterpret_cast<char *>(&car[i]), sizeof (car[i]));
@@ -43,6 +45,8 @@ bool checkbit(unsigned char BUFFER){
  * Legge dal file il codice binario che identifica l'albero.
  */
 bool * leggi_albero (istream &f4, unsigned char &BUFFER, int &l_buffer, int l_albero){
+	D1(cout<<"sono entrato in leggi_albero, BUFFER = "<<BUFFER<<" l_buffer = "<<
+	l_buffer<<" l_albero = "<<l_albero<<endl);
 	bool *albero = new bool [l_albero];
 	for (int i = 0; i<l_albero; i++){
 		if (l_buffer == 0){
@@ -63,6 +67,7 @@ bool * leggi_albero (istream &f4, unsigned char &BUFFER, int &l_buffer, int l_al
  * Ritorna un puntatore al nuovo elemento creato.
  */
 pnode crea_nodo (){
+	D2(cout<<"creo un nuovo nodo"<<endl);
 	pnode x = new nodo_t;
 	x->right = NULL;
 	x-> left = NULL;
@@ -78,28 +83,33 @@ pnode crea_nodo (){
  * La funzione ritorna un puntatore alla radice.
  */
 pnode costruisci_albero(const unsigned char caratteri[],const bool albero[],int l_albero){
-
+	D1(cout<<"sono entrato in costruisci_albero, caratteri = "<<caratteri<<" l_albero = "<<l_albero<<endl);
 	pnode root = crea_nodo();
 	pnode x = root;
 	int conta_car = 0;
 	for (int i = 0; i<l_albero; i++){
+		D2(cout<<"albero ["<<i<<"] = "<<albero[i]<<endl);
 		pnode temp = crea_nodo();
 		if (!albero[i]){
+			D2(cout<<"lo inserisco come figlio sinistro"<<endl);
 			temp->parent = x;
 			x->left = temp;
 			x = temp;
 			}
 		else
 			if (x->left == NULL){
+				D2(cout<<"scrivo la lettera nella foglia e risalgo finchè non trovo un ramo destro libero"<<endl);
 				x->carattere = caratteri[conta_car++];
 				x=x->parent;
 				while (x->right != NULL)
 					x = x->parent;
+				D2(cout<<"lo inserisco come figlio destro"<<endl);
 				temp->parent = x;
 				x->right = temp;
 				x = temp;
 				}
 			else{
+				D2(cout<<"lo inserisco come figlio destro"<<endl);
 				temp->parent = x;
 				x->right = temp;
 				x=temp;
@@ -115,6 +125,7 @@ pnode costruisci_albero(const unsigned char caratteri[],const bool albero[],int 
  * Ritorna un puntatore alla radice dell'albero
  */
 pnode crea_albero (istream &f4, unsigned char &BUFFER, int &l_buffer){
+	D1(cout<<"sono entrato in crea_albero, BUFFER = "<<BUFFER<<" l_buffer = "<<l_buffer<<endl);
 	int n_caratteri = numero_caratteri(f4);
 	unsigned char *caratteri = ricava_caratteri(f4, n_caratteri);
 	int l_albero=2*(n_caratteri-1);
@@ -129,33 +140,46 @@ pnode crea_albero (istream &f4, unsigned char &BUFFER, int &l_buffer){
 /** Decodifica carattere per carattere.
  *
  * La funzione scorre i bit del file compresso e li trasforma
- * grzie all'albero costruito precedentemente in caratteri che
+ * grazie all'albero costruito precedentemente in caratteri che
  * scrive sul file decompresso.
  */
 void decodifica(istream &f4,ostream &f5, unsigned char &BUFFER, int &l_buffer, pnode root){
+	D1(cout<<"sono entrato in scrivi_albero, BUFFER = "<<BUFFER<<" l_buffer = "<<l_buffer<<endl);
 	pnode x = root;
 	while (true){
 		if (l_buffer == 0){
+			D2(cout<<"il buffer è vuoto, leggo un carattere dal file compresso"<<endl);
 			f4.read(reinterpret_cast<char *>(&BUFFER), sizeof (BUFFER));
 			l_buffer = 8;
 			}
 		if (x->left == NULL && x->right == NULL){
-			if (x->carattere == 0)
+			D2(cout<<"ho trovato una foglia, quindi un carattere"<<endl);
+			if (x->carattere == 0){
+				D2(cout<<"è il terminatore, fine"<<endl);
 				return;
+				}
+			D2(cout<<"scrivo sul file decompresso il carattere e riparto dalla radice"<<endl);
 			f5<<x->carattere;
 			x = root;
 			}	
-		if(!checkbit(BUFFER))
+		if(!checkbit(BUFFER)){
+			D2(cout<<"scendo sul figlio sinistro"<<endl);
 			x=x->left;
-		else
+			}
+		else{
+			D2(cout<<"scendo sul figlio destro"<<endl);
 			x=x->right;
+			}
 		BUFFER = BUFFER<<1;	
 		l_buffer--;
+		D2(cout<<"ho shiftato il buffer, ora vale "<<static_cast <int>(BUFFER)<<
+		" e l_buffer vale "<<l_buffer<<endl);
 		}	
 }
 
 
 bool decomprimi(char sorgente[], char destinazione[]){
+	D1(cout<<"sono entrato in decomprimi, sorgente = "<<sorgente<<" destinazione = "<<destinazione<<endl);
 	ifstream f4(sorgente);
 	if (!f4)
 		return false;
